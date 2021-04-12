a1052bc65c839327cd26e4f9011f132b7a4639a5
Pedro Dias Marques <pmark.pt@gmail.com>
2021-04-12 16:23:35 +0100

    Project setup

---
+++ KD6-37/BaseKD637.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using ColorShapeLinks.Common;
+using ColorShapeLinks.Common.AI;
+using System;
+
+namespace KD6_37
+{
+    public class BaseKD637 : AbstractThinker
+    {
+		private List<FutureMove> possibleMoves;
+        private List<FutureMove> nonLosingMoves;
+        private Random random;
+
+        public override void Setup(string str)
+        {
+            possibleMoves = new List<FutureMove>();
+            nonLosingMoves = new List<FutureMove>();
+            random = new Random();
+        }
+
+        public override FutureMove Think(Board board, CancellationToken ct)
+        {
+            Winner winner;
+            PColor colorOfOurAI = board.Turn;
+
+            possibleMoves.Clear();
+            nonLosingMoves.Clear();
+
+            for (int col = 0; col < Cols; col++)
+            {
+                if (board.IsColumnFull(col)) continue;
+
+                for (int shp = 0; shp < 2; shp++)
+                {
+                    PShape shape = (PShape)shp;
+
+                    if (board.PieceCount(colorOfOurAI, shape) == 0) continue;
+
+                    possibleMoves.Add(new FutureMove(col, shape));
+
+                    board.DoMove(shape, col);
+
+                    winner = board.CheckWinner();
+
+                    // immediately
+                    board.UndoMove();
+
+                    if (winner.ToPColor() == colorOfOurAI)
+                    {
+                        return new FutureMove(col, shape);
+                    }
+                    else if (winner.ToPColor() != colorOfOurAI.Other())
+                    {
+                        nonLosingMoves.Add(new FutureMove(col, shape));
+                    }
+                }
+            }
+
+            if (nonLosingMoves.Count > 0)
+                return nonLosingMoves[random.Next(nonLosingMoves.Count)];
+
+            return possibleMoves[random.Next(possibleMoves.Count)];
+
+        }
+    }
+}
---
+++ TestKD6-37/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestKD6_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
